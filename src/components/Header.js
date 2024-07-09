7829335a6db2bcb72aa3312ecaefe9112d80b409
Sharath10kcoders <sharatheppalapally@10000coders.in>
2024-07-09 18:22:49 +0530

    next js app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1> Iam Header component</h1>
+        <h2>Lets start</h2>
+    </div>
+  )
+}
+
+export default Header
